44f81efaf5c3b4556ad88fac2126ef422175d548
joshscott95 <joshscott95@yahoo.com>
2024-09-02 17:40:26 -0700

    changed splash landing page updated index.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -12,8 +12,17 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    "Jakarta-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "Jakarta-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+    "Jakarta-ExtraLight": require("../assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
+    "Jakarta-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+    "Jakarta-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    "Jakarta-Regular": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
   });
+
+  const colorScheme = useColorScheme();
+
 
   useEffect(() => {
     if (loaded) {
@@ -26,10 +35,11 @@ export default function RootLayout() {
   }
 
   return (
-    
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+        <Stack.Screen name="not-found" />
       </Stack>
+    </ThemeProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,33 @@
-import { Text, View} from "react-native";
+import { Image, StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
     return (
-        <SafeAreaView>
-            <Text>Home</Text>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.imageContainer}>
+                <Image
+                    source={require('../assets/images/ryde.png')}
+                    style={styles.image}
+                    resizeMode="contain"
+                />
+            </View>
         </SafeAreaView>
     )
-
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    imageContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+    },
+});
 
 export default Home;
